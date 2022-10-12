587f6c6ad1986b04763f85e4ace30b8a9925fbea
Bulmang <hmk113737@gmail.com>
2022-10-12 20:30:56 +0900

    Initial Commit

---
+++ 3DShoeApp/_DShoeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DShoeAppApp.swift
+//  3DShoeApp
+//
+//  Created by 하명관 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct _DShoeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
